9a0e851c08da6e6606db40f3ea670a0fa167036e
driverfury <gionatanmerola@gmail.com>
2021-07-16 18:20:36 +0200

    Types and files input

--- src/ez.h
+++ src/ez.h
@@ -2,36 +2,65 @@
 #define EZ_H
 
 /******************************************************************************/
+/**                                  TYPES                                   **/
+/******************************************************************************/
+
+#include <inttypes.h>
+#include <stddef.h>
+
+/******************************************************************************/
 /**                                  GENERAL                                 **/
 /******************************************************************************/
 
 #define ez_min(x, y) (((x)<(y))?(x):(y))
 #define ez_max(x, y) (((x)>(y))?(x):(y))
 
+#ifndef NDEBUG
+#define ez_assert(cond)\
+    if(!(cond))\
+    {\
+        ez_abort();\
+    }
+#else
+#define ez_assert(cond) ((void *)0)
+#endif
+
+void ez_abort(void);
+
 /******************************************************************************/
 /**                                  MEMORY                                  **/
 /******************************************************************************/
 
-void  ez_mem_copy(void *src, void *dest, unsigned long bytes);
-void *ez_mem_alloc(unsigned long size);
+void  ez_mem_copy(void *src, void *dest, size_t bytes);
+void *ez_mem_alloc(size_t size);
 void  ez_mem_free(void *ptr);
-void *ez_mem_realloc(void *ptr, unsigned long size);
+void *ez_mem_realloc(void *ptr, size_t size);
 
 /******************************************************************************/
 /**                                 STRINGS                                  **/
 /******************************************************************************/
 
-unsigned long ez_str_len(char *s);
-unsigned long ez_str_len_max(char *s, unsigned long max);
-void          ez_str_copy(char *src, char *dest);
-void          ez_str_copy_max(char *src, char *dest, unsigned long max);
+size_t ez_str_len(char *s);
+size_t ez_str_len_max(char *s, size_t max);
+void   ez_str_copy(char *src, char *dest);
+void   ez_str_copy_max(char *src, char *dest, size_t max);
 
 /******************************************************************************/
-/**                                I/O & ERR                                 **/
+/**                              STD I/O & ERR                               **/
 /******************************************************************************/
 
 void ez_out_print(char *s);
 void ez_out_println(char *s);
+
+/******************************************************************************/
+/**                                 FILE I/O                                 **/
+/******************************************************************************/
+
+char  *ez_file_read_text(char *pathname, size_t *size);
+void  *ez_file_read_bin(char *pathname, size_t *size);
+void   ez_file_free(void *file_content);
+
+#define ez_file_read ez_file_read_text
 
 #endif
 
@@ -56,7 +85,13 @@ int _fltused = 0;
 #include <windows.h>
 
 void
-ez_mem_copy(void *src, void *dest, unsigned long bytes)
+ez_abort(void)
+{
+    ExitProcess(1);
+}
+
+void
+ez_mem_copy(void *src, void *dest, size_t bytes)
 {
     unsigned int i;
 
@@ -73,14 +108,14 @@ ez_mem_copy(void *src, void *dest, unsigned long bytes)
 
 typedef struct
 {
-    unsigned long size;
+    size_t size;
 } ez_allochdr;
 
 #define ez_get_allochdr(ptr)\
     ((ez_allochdr *)((char *)ptr - sizeof(ez_allochdr)))
 
 void *
-ez_mem_alloc(unsigned long size)
+ez_mem_alloc(size_t size)
 {
     ez_allochdr *hdr;
     void *ptr = 0;
@@ -107,11 +142,11 @@ ez_mem_free(void *ptr)
 }
 
 void *
-ez_mem_realloc(void *ptr, unsigned long size)
+ez_mem_realloc(void *ptr, size_t size)
 {
     void *newptr = 0;
     ez_allochdr *oldhdr;
-    unsigned long min_size;
+    size_t min_size;
     if(ptr)
     {
         if(size == 0)
@@ -129,10 +164,10 @@ ez_mem_realloc(void *ptr, unsigned long size)
     return(newptr);
 }
 
-unsigned long
+size_t
 ez_str_len(char *s)
 {
-    unsigned long len = 0;
+    size_t len = 0;
     while(*s++)
     {
         ++len;
@@ -140,10 +175,10 @@ ez_str_len(char *s)
     return(len);
 }
 
-unsigned long
-ez_str_len_max(char *s, unsigned long max)
+size_t
+ez_str_len_max(char *s, size_t max)
 {
-    unsigned long len = 0;
+    size_t len = 0;
     while(*s++ && len < max)
     {
         ++len;
@@ -161,9 +196,9 @@ ez_str_copy(char *src, char *dest)
 }
 
 void
-ez_str_copy_max(char *src, char *dest, unsigned long max)
+ez_str_copy_max(char *src, char *dest, size_t max)
 {
-    unsigned long i = 0;
+    size_t i = 0;
     while(*src && i < max)
     {
         *dest++ = *src++;
@@ -182,7 +217,7 @@ ez_out_print(char *s)
         _ez_stdout = GetStdHandle(STD_OUTPUT_HANDLE);
     }
 
-    WriteFile(_ez_stdout, s, ez_str_len(s), 0, 0);
+    WriteFile(_ez_stdout, s, (DWORD)ez_str_len(s), 0, 0);
 }
 
 void
@@ -190,6 +225,86 @@ ez_out_println(char *s)
 {
     ez_out_print(s);
     ez_out_print("\n");
+}
+
+char *
+ez_file_read_text(char *pathname, size_t *size)
+{
+    /* TODO */
+    char *content = 0;
+    char *content_ptr = 0;
+    HANDLE file_handle;
+    LARGE_INTEGER large_int;
+    size_t bytes_to_read;
+    uint32_t to_read, read;
+    int error_occurred;
+
+    *size = 0;
+
+    file_handle = CreateFileA(
+        pathname, GENERIC_READ, 0, 0, OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL, 0);
+    if(file_handle == INVALID_HANDLE_VALUE)
+    {
+        return(content);
+    }
+
+    if(GetFileSizeEx(file_handle, &large_int))
+    {
+        *size = large_int.QuadPart;
+
+        content = (char *)ez_mem_alloc(*size);
+        if(content)
+        {
+            bytes_to_read = *size;
+            error_occurred = 0;
+            content_ptr = content;
+            do
+            {
+                if(bytes_to_read <= UINT32_MAX)
+                {
+                    /* TODO: */
+                    to_read = (uint32_t)bytes_to_read;
+                }
+                else
+                {
+                    to_read = UINT32_MAX;
+                }
+                if(ReadFile(
+                    file_handle, content_ptr,
+                    to_read, (LPDWORD)&read, 0))
+                {
+                    bytes_to_read -= read;
+                    content_ptr += read;
+                }
+                else
+                {
+                    error_occurred = 1;
+                    ez_mem_free(content);
+                    content = 0;
+                    *size = 0;
+                }
+            } while((bytes_to_read > 0) && !(error_occurred));
+        }
+
+        CloseHandle(file_handle);
+    }
+
+    return(content);
+}
+
+void *
+ez_file_read_bin(char *pathname, size_t *size)
+{
+    void *content = 0;
+    content = (void *)ez_file_read_text(pathname, size);
+    return(content);
+}
+
+void
+ez_file_free(void *file_content)
+{
+    ez_mem_free(file_content);
 }
 
 #else
